c2157af0981a20203cf6622f095685a5d4e7799f
ariririri <fatal.t.h.f.flydream.bamboo@gmail.com>
2020-05-22 09:54:32 +0900

    add message

--- aizu/GRL_1_A.py
+++ aizu/GRL_1_A.py
@@ -9,12 +9,16 @@ def dijkstra(edges, s):
     d = [INF for x in range(V)]
     d[s] = 0
     que = []
+    # 原点からの距離, ノードをpush
     heapq.heappush(que, (0,s))
     while len(que) > 0:
-        u_d,u = heapq.heappop(que)
+        u_d, u = heapq.heappop(que)
+        # 既に最適化されたもの
         if d[u] < u_d:
             continue
-        for  v_d,v in edges[u]:
+        # uへの距離が確定したノード
+        for v_d, v in edges[u]:
+            # 今よりより距離があれば更新
             if d[v] > d[u] + v_d:
                 d[v] = d[u] + v_d
                 heapq.heappush(que,(d[v],v))
@@ -23,7 +27,7 @@ def dijkstra(edges, s):
 cost = [[] for i in range(V)]
 #cost[u][v] : 辺uvのコスト(存在しないときはinf この場合は10**10)
 for i in range(E):
-    x,y,z = map(int,input().split())
+    x, y, z = map(int,input().split())
     cost[x].append((z, y))
     #cost[y][x] = z
 d = dijkstra(cost, r)
